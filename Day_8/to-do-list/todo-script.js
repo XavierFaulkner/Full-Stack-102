1a24b4906da369be7ebb2ed509cf100239f531cb
Xavier Faulkner <xjfaulkner@gmail.com>
2022-03-31 08:34:22 -0400

    added to-do list

---
+++ Day_8/to-do-list/todo-script.js
@@ -0,0 +1,21 @@
+const list = document.querySelector('ul');
+const listItems = list.children;
+const inputBox = document.getElementById('inputTask');
+
+function addTask() {
+    if(inputBox.value != "") {
+        let newTask = document.createElement('li');
+        newTask.innerText = inputBox.value;
+        list.appendChild(newTask);
+        inputBox.value = "";
+    } else {
+        window.alert("Please enter a value into the text box.");
+        inputBox.focus();
+    }
+}
+
+list.addEventListener('click', function(e){
+    var listItem = e.target;
+    listItem.remove();
+});
+
