20b962bbac62718c6ef1d4b79991d6b4b3bd2a1e
XavierFaulkner <xjfaulkner@gmail.com>
2022-05-06 15:41:03 -0400

    updates

---
+++ Day_35/react-getting-started/part2/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>;
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button text={"plus"} onClick={increaseByOne}/>
+      <Button text={"zero"} onClick={setToZero}/>
+      <Button text={"minus"} onClick={decreaseByOne}/>
+    </div>
+  );
+}
+
+export default App
